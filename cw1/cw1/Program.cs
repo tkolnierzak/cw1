753971b532ae4f2c7c90484f13ae11f49617db5c
Tomasz Kołnierzak <s14855@pjwstk.edu.pl>
2020-03-01 19:06:18 +0100

    Task - GetEmails

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace cw1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var emails = await GetEmails(args[0]);
+
+            foreach(var a in args)
+            {
+                Console.WriteLine(a);
+            }
+
+            foreach (var email in emails)
+            {
+                Console.WriteLine(email);
+            }
+        }
+
+        static async Task<IList<string>> GetEmails(string url)
+        {
+            var httpClient = new HttpClient();
+            var listOfEmails = new List<string>();
+
+            var response = await httpClient.GetAsync(url);
+
+            Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*",
+           RegexOptions.IgnoreCase);
+            //find items that matches with our pattern
+            MatchCollection emailMatches = emailRegex.Matches(response.Content.ReadAsStringAsync().Result);
+
+        
+
+            foreach (var emailMatch in emailMatches)
+            {
+                listOfEmails.Add(emailMatch.ToString());
+            }
+
+            return listOfEmails;
         }
     }
 }
